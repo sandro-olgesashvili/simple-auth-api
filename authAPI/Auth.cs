83b607deb11430e7f92890f4573b7acea7dd86b3
sandro-olgesashvili <>
2023-01-08 17:00:55 +0400

    add project

---
+++ authAPI/Auth.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace authAPI
+{
+	public class Auth
+	{
+		public string Username { get; set; }
+		public string Password { get; set; }
+	}
+}
+
---
+++ authAPI/Controllers/authController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace authAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class authController : Controller
+    {
+        private static List<Auth> auths = new List<Auth>
+        {
+            new Auth{Username = "giorgi", Password= "123456"},
+            new Auth{Username = "daviti", Password= "123456"},
+        };
+
+        [HttpPost]
+        public async Task<ActionResult<bool>> Post([FromBody]Auth authRequest)
+        {
+            var auth = auths.Find((i) => i.Username == authRequest.Username);
+
+            if(auth.Password != authRequest.Password)
+            {
+                return BadRequest(false);
+            }
+
+            return Ok(true);
+        }
+    }
+}
+
