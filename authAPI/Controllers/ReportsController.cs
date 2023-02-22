676e6b6f3900a99002cfbe36e3acf79da5e55909
sandro-olgesashvili <>
2023-02-22 20:43:07 +0400

    update report

--- authAPI/Controllers/ReportsController.cs
+++ authAPI/Controllers/ReportsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using authAPI.Service;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -23,7 +24,7 @@ namespace authAPI.Controllers
             _userService = userService;
         }
 
-        [HttpGet]
+        [HttpGet, Authorize(Roles ="admin")]
         public async Task<ActionResult<bool>> Get()
         {
 
@@ -40,6 +41,25 @@ namespace authAPI.Controllers
 
             return Ok(productSales);
         }
+
+        [HttpGet("date"), Authorize(Roles ="admin")]
+        public async Task<ActionResult<bool>> GetByDate([FromQuery] DateTime start, [FromQuery] DateTime end)
+        {
+
+            var soldProducts = await _context.SoldProducts.Where(x => x.DateTime <= end && start <= x.DateTime).ToListAsync();
+
+            var soldProductsByProduct = soldProducts.GroupBy(sp => sp.ProductName);
+
+            var productSales = soldProductsByProduct.Select(g => new {
+                ProductName = g.Key,
+                TotalSales = g.Sum(sp => sp.Quantity * sp.Price),
+                Quantity = g.Sum(sp => sp.Quantity)
+            }).OrderByDescending(x => x.TotalSales).ToList();
+
+
+            return Ok(productSales);
+        }
+ 
     }
 }
 
